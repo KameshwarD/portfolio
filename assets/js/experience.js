fe20013d623da3b705d53d79d931ae2babb5f13e
Sri Kameshwar Dhulipala <kameshwardhulipala@gmail.com>
2025-07-06 02:18:53 -0400

    Experience Updated

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,25 +5,46 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Data Analyst",
+    cardImage: "assets/images/experience-page/acadia.logo.jpg",
+    place: "Acadia Technologies",
+    time: "Apr 2025 - present",
+    desp: "<li>Data Cleaning & Preparation – Cleaned and processed datasets by filtering 10,000+ records and resolving 15% missing values, achieving 95% data accuracy for ERP and software development projects</li><li>Data Visualization – Designed 5+ interactive dashboards using Tableau and Power BI, visualizing key metrics to enhance stakeholder decision-making and reduce analysis time by 20%</li><li>Team Collaboration – Collaborated with software and consulting teams to streamline data workflows, supporting 4+ stakeholders in enterprise integration initiatives</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    title: "Data Auditor - Intern",
+    cardImage: "assets/images/experience-page/cmslogo2.jpg",
+    place: "Illinois Department of Central Management Services",
+    time: "Jun 2024 - Dec 2024",
+    desp: "<li>Ensured Compliance and Data Accuracy – Audited the Division of Vehicles (DOV) fleet management system, verifying billing codes, asset numbers, and critical data to ensure compliance with JCAR and regulations, preventing discrepancies</li><li>Developed SQL queries to validate database records and assisted in creating test plans in TestRail, gaining practical experience with SDLC and web-based application testing</li><li>Cleared Overdue Audits – Completed data audits from 2018–2024 using SQL, Power BI, and Microsoft Excel, improving departmental productivity by 30% through efficient handling of high-priority tasks</li><li>Managed Fleet Data – Analyzed and maintained 10,000+ vehicle records, ensuring accurate order status tracking, timely updates, and better resource allocation</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Web Development Intern",
+    cardImage: "assets/images/experience-page/uis.png",
+    place: "University of Illinois Springfield",
+    time: "Oct 2023 - May 2024",
+    desp: "<li>Developed & Enhanced Digital Case Management - Designed and maintained a cloud-based mental health support platform on Azure, enabling seamless case tracking, counselor assignments, and session history management, ensuring scalability and accessibility</li><li>Data-Driven Decision Making - Built interactive dashboards using Tableau, Power BI & SQL, leveraging Analytics to analyze student engagement, issue severity, resolution times, and counselor efficiency, providing actionable insights to improve support services</li><li>Ensured Compliance & Data Privacy - Implemented secure authentication protocols and data encryption to protect student mental health records, ensuring compliance with university policies</li><li>Collaborated with Counselors & Support Teams - Worked closely with students, student counselors, and university administrators to gather requirements, refine workflows, and develop user-friendly features for the platform</li>",
+  },
+  {
+    title: "Systems Engineer",
+    cardImage: "assets/images/experience-page/infosyslogo.png",
+    place: "Infosys Ltd",
+    time: "Mar 2022 - Aug 2023",
+    desp: "<li>Strengthened Core Banking Operations – Assisted in the FINACLE implementation, deployed in 100 countries, supporting 1.3 billion bank accounts and ensuring seamless transaction processing</li><li>Optimized Financial Closing Processes – Provided business and technical analysis for year-end financial closing in African banks, ensuring data integrity, and process efficiency, contributing to FINACLE’s $700M revenue in FY 2022-23</li><li>Improved Data Accuracy & System Performance – Conducted Level-3 system testing and root cause analysis, leveraging SQL and Azure DevOps to identify and resolve critical database discrepancies, enhancing data integrity and operational efficiency</li><li>Developed Data-Driven Issue Management Dashboards – Designed interactive Power BI dashboards for tracking and analyzing bank-reported issues, segmenting by account type, severity, and resolution status. Utilized Azure DevOps for tracking resolution timelines, identifying bottlenecks, and optimizing response efficiency through data-driven decision-making</li><li>Business Process Analysis & Optimization – Mapped and analyzed core banking workflows using ARIS and Visio, identifying inefficiencies and recommending process enhancements to streamline operations</li>",
+  },
+  {
+    title: "Analyst A4",
+    cardImage: "assets/images/experience-page/capgemini-logo.png",
+    place: "Capgemini Technology Services",
+    time: "Jun 2021 - Feb 2022",
+    desp: "<li>Optimized data models and reporting using SQL, Python, Tableau, and Power BI to enhance BaNCS core banking processes, improving transaction efficiency, compliance tracking, and decision-making</li><li>Led the implementation of BaNCS ERP modules with Excel, Azure, and Power BI, driving improvements in banking operations, customer management, and risk assessment</li><li>Created stories and managed work by using Azure DevOps Boards</li><li>Streamlined business processes using ARIS, Visio, and SharePoint, ensuring effective collaboration between business and technical teams to enable automation and system enhancements</li>",
+  },
+  {
+    title: "Intern",
+    cardImage: "assets/images/experience-page/bhel_logo.png",
+    place: "Bharat Heavy Electricals Limited",
+    time: "May 2018 - Jun 2019",
+    desp: "<li>Produced 5+ reports on surveying methodologies and performed comparison analysis by examining unusual trends for 1000 employees across 5 different manufacturing facilities in the organization</li><li>Developed microservices-based inventory management system for data management utilizing Docker and Kubernetes, improving data management reliability for Steam Turbines, Wind Turbines, and Pulverizers by 25% and reducing stockouts of manufacturing material by 30%</li>"
   },
 ];
 
